f865fdf72c8e995e6321fa7db62a0e64db85d28f
mlucero <mauricio.lucero2014@gmail.com>
2021-03-31 00:43:03 -0300

    chore: add database connection, user model and server confg

--- models/server.ts
+++ models/server.ts
@@ -1,5 +1,38 @@
 import express, { Application } from 'express';
+import path from 'path';
+import cors from 'cors';
+import winston from 'winston';
+
 import usersRoutes from '../api/resources/users/users.routes';
+import db from '../db/connection';
+
+const includeDate = winston.format(info => {
+	return {
+		...info,
+		message: `${new Date().toISOString()} ${info.message}`
+	};
+});
+
+const logger = winston.createLogger({
+	transports: [
+		new winston.transports.Console({
+			level: 'debug',
+			handleExceptions: true,
+			format: winston.format.combine(
+				winston.format.colorize(),
+				winston.format.simple()
+			)
+		}),
+		new winston.transports.File({
+			level: 'info',
+			handleExceptions: true,
+			format: winston.format.combine(includeDate(), winston.format.simple()),
+			maxsize: 5120000, //5mb
+			maxFiles: 5,
+			filename: path.join(__dirname, '../logs/app.log')
+		})
+	]
+});
 
 export class Server {
 	private app: Application;
@@ -12,8 +45,34 @@ export class Server {
 		this.app = express();
 		this.port = process.env.PORT || '8000';
 
-		//Load routes
-		this.routes();
+		//db
+		this.dbConnection();
+
+		//Middlewares
+		this.setupMiddlewares();
+
+		//Register routes
+		this.registryRoutes();
+	}
+
+	async dbConnection(): Promise<void> {
+		try {
+			await db.authenticate();
+			logger.info('Database online');
+		} catch (error) {
+			throw new Error(error);
+		}
+	}
+
+	setupMiddlewares(): void {
+		//CORS
+		this.app.use(cors());
+
+		//BodyParser
+		this.app.use(express.json());
+
+		//Public resources
+		this.app.use(express.static('public'));
 	}
 
 	listen(): void {
@@ -22,7 +81,7 @@ export class Server {
 		});
 	}
 
-	routes(): void {
+	registryRoutes(): void {
 		this.app.use(this.apiPaths.users, usersRoutes);
 	}
 }
